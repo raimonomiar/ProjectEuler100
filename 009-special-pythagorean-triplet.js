2e1b32c5ca44517ab6578a039de2b7ef2507b977
Ayush Karki <raimonomiar@gmail.com>
2020-11-02 22:56:31 +0545

    009 problem optimized

--- 009-special-pythagorean-triplet.js
+++ 009-special-pythagorean-triplet.js
@@ -7,20 +7,14 @@ var breakFromLoop = false
 while( a <= limit && !breakFromLoop) {
     var b = 1;
     while (b <= limit && !breakFromLoop) {
-        var c = 1;
-        while (c <= limit && !breakFromLoop) {
-
-            if ((Math.pow(a, powerValue) + Math.pow(b, powerValue)) === Math.pow(c, powerValue)) {
-                
-                if (a + b + c === limit ) {
-                    console.log(a * b * c);
-                    console.log(a,b,c)
-                    breakFromLoop = true
-                }
-            }
-            c++;
+        
+        var c = Math.sqrt(Math.pow(a,powerValue) + Math.pow(b, powerValue));
+        if (a + b + c === limit) {
+            console.log(a * b  * c);
+            breakFromLoop = true;
         }
-        b++
+        
+        b++;
     }
     a++;
 }
